da8dcafaf25e403ecbb2611f2b793be47fe7b3fd
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-09 19:23:43 +0500

    class practice

--- index.php
+++ index.php
@@ -1,56 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
+Interface
+Interfaces permit users to programmatically define the public methods that a class should implement without bothering about the implementation complexities and details of any particular method. Interfaces make it easy to use different classes in the same way. When one or more classes use the same interface, it is referred to as "polymorphism" in PHP.
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+Interfaces are declared with interface keyword.
 
-<body>
-
-  <?php
-
-  // Interface
-  // Interfaces permit users to programmatically define the public methods that a class should implement without bothering about the implementation complexities and details of any particular method. Interfaces make it easy to use different classes in the same way. When one or more classes use the same interface, it is referred to as "polymorphism" in PHP.
-
-  // Interfaces are declared with interface keyword. 
-
-  // Example:
+Example:
 
 
 
-  interface InterfaceName
+<?php
+interface InterfaceName
+{
+  public function Method1();
+  public function Method2($name, $surname);
+  public function Method3(): string;
+}
+?>
+
+
+Implementing Interface
+To implement an interface on a class, we need to use implements keyword.
+
+Example:
+
+
+
+<?php
+interface Human
+{
+  public function makeSound();
+}
+
+class Programmer implements Human
+{
+  public function makeSound()
   {
-    public function Method1();
-    public function Method2($name, $surname);
-    public function Method3(): string;
+    echo "Hello World";
   }
+}
 
-
-  //    Implementing Interface
-  //  To implement an interface on a class, we need to use implements keyword. 
-
-  // Example:
-
-  interface Human
-  {
-    public function makeSound();
-  }
-
-  class Programmer implements Human
-  {
-    public function makeSound()
-    {
-      echo "Hello World";
-    }
-  }
-
-  $human = new Programmer();
-  $human->makeSound();
-
-  ?>
-</body>
-
-</html>
+$human = new Programmer();
+$human->makeSound();
+?>
